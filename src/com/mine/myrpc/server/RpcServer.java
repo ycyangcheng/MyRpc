9888d5e4e467cb4f87708d8004c5354ee580ecc4
ycyangcheng <yangcheng317@163.com>
2020-06-05 14:34:55 +0800

    初始化项目

---
+++ src/com/mine/myrpc/server/RpcServer.java
@@ -0,0 +1,12 @@
+package com.mine.myrpc.server;
+
+/**
+ * @author: Yang Cheng
+ * @create: 2020-06-05 14:17
+ * @desc:
+ */
+public class RpcServer {
+    public static void main(String[] args) {
+        System.err.println("======");
+    }
+}
